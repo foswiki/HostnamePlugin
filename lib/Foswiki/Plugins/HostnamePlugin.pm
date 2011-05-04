349febe5bf49e622d7ef0c3c0e549a3d658d418c
AndrewJones <AndrewJones@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-05-04 15:06:45 +0000

    Item10708: Non-closing span on error message
    git-svn-id: http://svn.foswiki.org/trunk/HostnamePlugin@11628 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/HostnamePlugin.pm
+++ lib/Foswiki/Plugins/HostnamePlugin.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2007 - 2009 Andrew Jones, andrewjones86@googlemail.com
+# Copyright (C) 2007 - 2011 Andrew Jones, http://andrew-jones.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -16,24 +16,15 @@
 package Foswiki::Plugins::HostnamePlugin;
 
 use strict;
-
-use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
+use warnings;
 
 our $VERSION = '$Rev$';
-our $RELEASE = '1.0';
+our $RELEASE = '1.1';
 our $SHORTDESCRIPTION = 'Displays the hostname of the server serving Foswiki.';
 our $NO_PREFS_IN_TOPIC = 1;
 our $pluginName = 'HostnamePlugin';
 
 sub initPlugin {
-    #my( $topic, $web, $user, $installWeb ) = @_;
-
-    # check for Plugins.pm versions
-    if( $Foswiki::Plugins::VERSION < 1 ) {
-        Foswiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
-        return 0;
-    }
-
     Foswiki::Func::registerTagHandler( 'HOSTNAME', \&_handleTag);
 
     # Plugin correctly initialized
@@ -48,7 +39,7 @@ sub _handleTag {
         return hostname();
     } else {
         Foswiki::Func::writeWarning( $@ );
-        return "<span class='foswikiAlert'>$pluginName error: Sys::Hostname not found in \@INC. Please contact %WIKIWEBMASTER%.";
+        return "<span class='foswikiAlert'>$pluginName error: Sys::Hostname not found in \@INC. Please contact %WIKIWEBMASTER%.</span>";
     }
 }
 
